1cc3731b5d9e828a85326346b7626844f87345f4
Burak Karakan <burak.karakan@boosmart.com>
2017-05-05 15:28:15 +0300

    Added more comments.

--- SubdomainGenerator.sh
+++ SubdomainGenerator.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #
-# Nginx Bash script for generating new subdomain with a new server block.
+# Bash script for generating new subdomain with a new server block in Nginx.
 
 # Functions
 ok() { echo -e '\e[32m'$1'\e[m'; } # Green
 die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
 
-# Variables
+# Variable definitions.
 NGINX_AVAILABLE_VHOSTS='/etc/nginx/sites-available'
 NGINX_ENABLED_VHOSTS='/etc/nginx/sites-enabled'
 WEB_DIR='/var/www'
 NGINX_SCHEME='$scheme'
 NGINX_REQUEST_URI='$request_uri'
 
-# Sanity check
+# Sanity check.
 [ $(id -g) != "0" ] && die "Script must be running as root."
 [ $# != "2" ] && die "Usage: $(basename $0) subDomainName mainDomainName"
 
 ok "Creating the config files for your subdomain."
 
-# Create nginx config file
+# Create the Nginx config file.
 cat > $NGINX_AVAILABLE_VHOSTS/$1 <<EOF
 server {
     # Just the server name
@@ -39,10 +39,10 @@ server {
 }
 EOF
 
-# Creating {public,log} directories
+# Create {public,log} directories.
 mkdir -p $WEB_DIR/$1/{public_html,logs}
 
-# Creating index.html file
+# Create index.html file.
 cat > $WEB_DIR/$1/public_html/index.html <<EOF
 <!DOCTYPE html>
 <html lang="en">
@@ -61,14 +61,13 @@ cat > $WEB_DIR/$1/public_html/index.html <<EOF
 </html>
 EOF
 
-# Changing permissions
+# Change the folder permissions.
 chown -R $USER:$WEB_USER $WEB_DIR/$1
 
-# Enable site by creating symbolic link
+# Enable site by creating symbolic link.
 ln -s $NGINX_AVAILABLE_VHOSTS/$1 $NGINX_ENABLED_VHOSTS/$1
 
-# Restart
-
+# Restart the Nginx server.
 read -p "A restart to Nginx is required for the subdomain to be defined. Do you wish to restart nginx? (y/n): " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
